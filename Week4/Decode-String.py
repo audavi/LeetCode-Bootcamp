fafc362a43e6328b77690a5ac4497b2783161b81
Emily Lopez <161864960+audavi@users.noreply.github.com>
2025-03-23 22:31:33 -0400

    Add files via upload

---
+++ Week4/Decode-String.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        for char in s:
+            if char != ']':
+                stack.append(char)
+            else:
+                curr = ""
+                while stack and stack[-1] != '[':
+                    curr = stack.pop() + curr
+                stack.pop()
+                num = ""
+                while stack and stack[-1].isdigit():
+                    num = stack.pop() + num
+                stack.append(curr * int(num))
+        return "".join(stack)
+
